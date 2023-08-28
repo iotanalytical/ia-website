bcb679ff2d69140d62bf5a16255e4e7ab93e86d4
Ali Sadoughi <asadoughi1990@gmail.com>
2023-08-28 13:15:55 -0400

    navbar structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
-    <main className="App">
-      <h1>IoT Analytical App</h1>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,11 @@
+import NavbarLanding from "../../components/NavbarLanding";
+
+const Landing = () => {
+  return (
+    <>
+      <NavbarLanding />
+    </>
+  );
+};
+
+export default Landing;
