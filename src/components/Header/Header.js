465b1e67cc0c756b34d00c988c3e01c9d1f9becd
Ali Sadoughi <asadoughi1990@gmail.com>
2023-11-09 17:07:19 -0500

    fix a bug in header link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,26 @@ import Button from "react-bootstrap/Button";
 
 import heroImage from "../../assets/images/banner.jpeg";
 
-const Header = ({ failedAuth }) => {
+import { useNavigate } from "react-router-dom";
+
+const Header = ({ userName, failedAuth }) => {
+  const navigate = useNavigate();
+
   const handleDemoClick = () => {
-    document.getElementById("contact-us").scrollIntoView({
-      behavior: "smooth",
-    });
+    if (!failedAuth) {
+      navigate(`/dashboard/${userName}`);
+    } else {
+      document.getElementById("contact-us").scrollIntoView({
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
     <section id="home" className="header">
       <div className="header__info">
         <h1 className="header__title">Analytics Solution for Smart Homes</h1>
-        
+
         <Button
           className="header__btn"
           onClick={handleDemoClick}
