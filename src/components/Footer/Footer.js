2161f171f5fc96a220839dbd6b5b06e21c877649
Ali Sadoughi <asadoughi1990@gmail.com>
2023-11-09 19:39:08 -0500

    footer modification

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,44 @@
-import './Footer.scss';
+import "./Footer.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <p className="footer__text">© IoT Analytical Inc. All Rights Reserved.</p>
-        </footer>
-    );
+  const date = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="social">
+        <a
+          href="mailto:info@iotanalytical.com?Subject=Need to be smarter"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Email"
+        >
+          <i className="fa fa-envelope social__icon"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/company/iotanalytical"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="linkedin"
+        >
+          <i className="fa fa-linkedin social__icon"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/iotanalytical"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="instagram"
+        >
+          <i className="fa fa-instagram social__icon"></i>
+        </a>
+      </div>
+
+      <p className="footer__text">
+        © {date} IoT Analytical Inc. All Rights Reserved.
+      </p>
+
+      <p className="footer__text">350-128 W.Hastings, Vancouver, BC</p>
+    </footer>
+  );
 };
 
 export default Footer;
